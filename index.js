619a1f684d6763dc035ee05a51841031b139fd94
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-11-23 11:49:37 -0500

    commit tree 65db5c1b6fb2a65ffe3863f057b0209b53671013

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee ={
+    name: 'John',
+    streetAddress: '45 Stop Now Dr.',
+}
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value){
+    return {
+        ...employeeObj,
+        [key]: value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value){
+    employeeObj[key] = value;
+    return employeeObj;
+}
+
+function deleteFromEmployeeByKey(employeeObj, key){
+    const keyLess = {...employeeObj};
+    delete keyLess[key];
+    return keyLess;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key){
+    delete employeeObj[key];
+    return employeeObj;
+}
